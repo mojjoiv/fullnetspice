abe6aaac1f0b9af9298fac6907b5afeb2f8574ea
mojjoiv <daltonongeche@gmail.com>
2024-02-09 13:50:50 +0300

    updated the ui

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import toast, { Toaster } from 'react-hot-toast';
+import toast from 'react-hot-toast';
 import { useFormik } from 'formik';
 import { getFormData } from './formData.js';
 import { profileValidation } from '../helper/validate';
--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import avatar from '../assets/profile.png';
 import { Toaster } from 'react-hot-toast';
 import { useFormik } from 'formik';
 import { usernameValidate } from '../helper/validate'
